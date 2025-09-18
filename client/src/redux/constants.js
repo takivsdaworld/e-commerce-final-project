a0c200ca97af175b23c2787305e8b6848bcfe300
takivsdaworld <164879377+takivsdaworld@users.noreply.github.com>
2025-09-18 14:56:21 +0200

    issues issues issues

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -1,8 +1,8 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || "";
-export const USERS_URL = "/users";
-export const CATEGORY_URL = "/category";
-export const PRODUCT_URL = "/products";
-export const UPLOAD_URL = "/upload";
-export const ORDERS_URL = "/orders";
-export const DASHBOARD_URL = "/dashboard";
+export const BASE_URL = "";
+export const USERS_URL = "/api/users";
+export const CATEGORY_URL = "/api/category";
+export const PRODUCT_URL = "/api/products";
+export const UPLOAD_URL = "/api/upload";
+export const ORDERS_URL = "/api/orders";
+export const DASHBOARD_URL = "/api/dashboard";
 
